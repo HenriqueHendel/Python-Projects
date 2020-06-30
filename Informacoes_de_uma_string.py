68ba930733360d1db545d193eb34bede94d81de3
Henrique Hendel <henrique.henriquelopess@gmail.com>
2020-06-29 23:41:45 -0300

    Programa que retorna algumas informações de uma string

---
+++ Informacoes_de_uma_string.py
@@ -0,0 +1,8 @@
+string = input("Digite uma frase: ")
+print("É alfebético ?", string.isalpha())
+print("É numérico ?", string.isnumeric())
+print("É alfanumérico ?", string.isalnum())
+print("Está em maiúsculas ?", string.isupper())
+print("Está em minúsculas ?", string.islower())
+print("Está começando com letra maiúscula (capitalizado) ?", string.istitle())
+print("Possui apenas espaços? ", string.isspace())
